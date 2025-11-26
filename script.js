35f1fd72ecae15ae65df2885d9d09285ba694ea1
RAYNALD HOUDE <raynald.houde2311@gmail.com>
2025-11-26 12:10:35 -0500

    Enhance changeColor function to update text color
    Added color change for text when square color changes.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Fonction qui change la couleur du carré au clic
+// Fonction qui change la couleur du carré au clic simple
 function changeColor(square) {
     // Récupérer la couleur actuelle du carré
     const currentColor = square.style.backgroundColor;
@@ -6,10 +6,13 @@ function changeColor(square) {
     // Changer la couleur en fonction de la couleur actuelle
     if (currentColor === 'green') {
         square.style.backgroundColor = 'yellow'; 
+        square.style.color = 'black';
     } else if (currentColor === 'yellow') {
         square.style.backgroundColor = 'red';  
-    } else if (currentColor === 'red') {
+        square.style.color = 'black';
+         } else if (currentColor === 'red') {
         square.style.backgroundColor = 'green';
+         square.style.color = 'black';
     } else {
         square.style.backgroundColor = 'green';
     }
